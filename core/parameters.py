4f88e502c017e8ca407397a552b53a36551cf1a7
TheRealCambu <s324564@studenti.polito.it>
2024-11-04 12:50:20 +0100

    The speed of light was wrong!

--- core/parameters.py
+++ core/parameters.py
@@ -4,5 +4,5 @@ import scipy.constants as con
 # Use this file to define all your input parameters (e.g. lengths, loss coefficient, etc.)
 
 # c = speed of light
-c = con.h
+c = con.c
 c_fiber = c * 2 / 3
